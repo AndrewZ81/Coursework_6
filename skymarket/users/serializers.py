10ed91fea11f5acbda6c99a3274708a978ace361
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-24 15:35:19 +0300

    Создал сериализаторы "UserRegistrationSerializer", 'UserSerializer'

--- skymarket/users/serializers.py
+++ skymarket/users/serializers.py
@@ -7,9 +7,23 @@ User = get_user_model()
 # TODO для создания пользователя из за того, что у нас имеются нестандартные поля
 
 
-class UserRegistrationSerializer(BaseUserRegistrationSerializer):
-    pass
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    def is_valid(self, *, raise_exception=False):
+        return super().is_valid(raise_exception=raise_exception)
+
+    def create(self, validated_data):
+        new_user = User.objects.create(**validated_data)
+        new_user.set_password(validated_data["password"])
+        new_user.save()
+        return new_user
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "phone", "email", "password", "image"]
 
 
-class CurrentUserSerializer(serializers.ModelSerializer):
-    pass
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "phone", "email", "image"]
