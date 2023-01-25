d26431f2264f3c3a4bd52761b584c456ebf91906
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-25 20:18:08 +0300

    Создал путь 'create/'

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
-from .views import AdListView, AdDetailView, AdMyListView
+from .views import AdListView, AdDetailView, AdMyListView, AdCreateView
 
 urlpatterns = [
     path('', AdListView.as_view(), name="ads_list"),
+    path('create/', AdCreateView.as_view(), name="ad_create"),
     path('<int:pk>/', AdDetailView.as_view(), name="ad_detail"),
-    path('me/', AdMyListView.as_view(), name="my_ads_list")
+    path('me/', AdMyListView.as_view(), name="my_ads_list"),
+
 ]
