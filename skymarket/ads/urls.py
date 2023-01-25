930249f8b54bee11d8eefe8e392065432c4735c4
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-25 14:18:05 +0300

    Добавил пути <int:pk>/', 'me/'

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
-from .views import AdListView
+from .views import AdListView, AdDetailView, AdMyListView
 
 urlpatterns = [
     path('', AdListView.as_view(), name="ads_list"),
+    path('<int:pk>/', AdDetailView.as_view(), name="ad_detail"),
+    path('me/', AdMyListView.as_view(), name="my_ads_list")
 ]
