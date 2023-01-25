2e6833f7074a64bd4d3edf532e8724f44e0bdddd
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-25 14:17:00 +0300

    Создал сериализатор 'AdDetailViewSerializer'

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -1,12 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
+from rest_framework.fields import SerializerMethodField
 
 from .models import Ad
+
+User = get_user_model()
 
 
 class AdListViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         exclude = ["author", "created_at"]
+
+
+class AdDetailViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        exclude = ["created_at", "author"]
+
+    author_id = PrimaryKeyRelatedField(queryset=User.objects.all())
+    author_first_name = SerializerMethodField()
+    author_last_name = SerializerMethodField()
+    phone = SerializerMethodField()
+
+
+    def get_author_first_name(self, ad):
+        return ad.author.first_name
+
+    def get_author_last_name(self, ad):
+        return ad.author.last_name
+
+    def get_phone(self, ad):
+        return str(ad.author.phone)
 
 
 class CommentSerializer(serializers.ModelSerializer):
