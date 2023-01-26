d32960a101c79596a50bec3160c1dd9b9064d287
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-26 17:25:08 +0300

    Создал CBV 'CommentListCreateView'

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from .permissions import AdRetrieveUpdateDestroyPermission
 
 from .models import Ad, Comment
-from .serializers import AdListViewSerializer, AdDetailViewSerializer
+from .serializers import AdListViewSerializer, AdDetailViewSerializer, CommentListViewSerializer
 
 
 class AdListCreateView(ListCreateAPIView):
@@ -43,3 +43,21 @@ class AdRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdDetailViewSerializer
     permission_classes = [IsAuthenticated, AdRetrieveUpdateDestroyPermission]
+
+
+class CommentListCreateView(ListCreateAPIView):
+    """
+    - GET отображает все комментарии к объявлению, выбранному по id
+    - POST создаёт новый комментарий к объявлению, выбранному по id
+    """
+    serializer_class = CommentListViewSerializer
+
+    def get_ad_pk(self):
+        return self.kwargs["ad_pk"]
+
+    def get_queryset(self):
+        return Comment.objects.filter(ad_id=self.get_ad_pk())
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, ad_id=self.get_ad_pk())
