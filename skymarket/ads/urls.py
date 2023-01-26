aab75f22b1fdf0c1d2ce47e218c1e4bd9599d00f
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-26 20:12:43 +0300

    Добавил маршрут 'comments/<int:pk>'

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import AdMyListView, AdListCreateView, AdRetrieveUpdateDestroyView,\
-    CommentListCreateView
+from .views import AdMyListView, AdListCreateView, AdRetrieveUpdateDestroyView, \
+    CommentListCreateView, CommentRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', AdListCreateView.as_view(), name="ads_list"),
     path('<int:pk>/', AdRetrieveUpdateDestroyView.as_view(), name="ad_read_upd_del"),
     path('me/', AdMyListView.as_view(), name="my_ads_list"),
-    path('<int:ad_pk>/comments/', CommentListCreateView.as_view(), name="comment_list_create")
+    path('<int:ad_pk>/comments/', CommentListCreateView.as_view(), name="comment_list_create"),
+    path('comments/<int:pk>', CommentRetrieveUpdateDestroyView.as_view(), name="comment_read_upd_del")
 
 ]
