929ef352b2f919764f5394c48ccd6bac70d6dcd5
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-26 12:31:14 +0300

    Создал класс доступа 'AdRetrieveUpdateDestroyPermission'

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,17 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRoles
+
+
+class AdRetrieveUpdateDestroyPermission(BasePermission):
+    """
+    Разграничивает права доступа авторизованных пользователей
+    """
+    message = "Доступ разрешён только администратору или владельцу объявления"
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "GET":
+            return True
+        if request.user.role == UserRoles.ADMIN or request.user.id == obj.author_id:
+            return True
+        return False
