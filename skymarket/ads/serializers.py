ab67e2fdf735bf400e38b726be753025e34f33e4
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-26 17:22:55 +0300

    Создал сериализатор 'CommentListViewSerializer'

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.fields import SerializerMethodField
 
-from .models import Ad
+from .models import Ad, Comment
 
 User = get_user_model()
 
@@ -35,3 +35,35 @@ class AdDetailViewSerializer(serializers.ModelSerializer):
 
     def get_phone(self, ad):
         return str(ad.author.phone)
+
+
+class CommentListViewSerializer(serializers.ModelSerializer):
+    text = serializers.CharField(allow_blank=False, allow_null=False)
+    ad_id = SerializerMethodField(required=False)
+    author_id = SerializerMethodField(required=False)
+    author_first_name = SerializerMethodField()
+    author_last_name = SerializerMethodField()
+    image = SerializerMethodField()
+
+
+    class Meta:
+        model = Comment
+        exclude = ["ad", "author"]
+
+    def get_text(self, comment):
+        return comment.text
+
+    def get_ad_id(self, comment):
+        return comment.ad_id
+
+    def get_author_id(self, comment):
+        return comment.author_id
+
+    def get_author_first_name(self, comment):
+        return comment.author.first_name
+
+    def get_author_last_name(self, comment):
+        return comment.author.last_name
+
+    def get_image(self, comment):
+        return comment.author.image.url if comment.author.image else None
